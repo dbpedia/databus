0ac73a629de3ee37ae1a890e172227838336debc
Thorsten Sommer <thorsten.sommer@dlr.de>
2021-10-29 10:10:14 +0200

    Fixed proxy setup script

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@ echo "Waiting for database at ${DATABUS_DATABASE_URL}/sparql..."
 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' ${DATABUS_DATABASE_URL}/sparql)" != "200" ]]; do sleep 5; done'
 
 # Is the proxy server enabled?
-if [ $DATABUS_PROXY_SERVER_ENABLE = 'true' ]; then
+if [ "$DATABUS_PROXY_SERVER_ENABLE" = "true" ]; then
 	
     # Remove the previous Caddyfile:
     rm -f /etc/caddy/Caddyfile
 
     # Should we use ACME?
-    if [ $DATABUS_PROXY_SERVER_USE_ACME = 'true' ]; then
+    if [ "$DATABUS_PROXY_SERVER_USE_ACME" = "true" ]; then
         
         # Re-create the Caddyfile using the user's configuration:
-        $ cat <<EOF > /etc/caddy/Caddyfile
+        cat <<EOF > /etc/caddy/Caddyfile
 $DATABUS_PROXY_SERVER_HOSTNAME:3002 {
     reverse_proxy 127.0.0.1:3000
 }
@@ -25,7 +25,7 @@ EOF
     else
 
         # Re-create the Caddyfile using the user's configuration:
-        $ cat <<EOF > /etc/caddy/Caddyfile
+        cat <<EOF > /etc/caddy/Caddyfile
 $DATABUS_PROXY_SERVER_HOSTNAME:3002 {
     tls /tls/$DATABUS_PROXY_SERVER_OWN_CERT /tls/$DATABUS_PROXY_SERVER_OWN_CERT_KEY
     reverse_proxy 127.0.0.1:3000
