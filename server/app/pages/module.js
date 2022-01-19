e82129ad2b6be61162cdd31e5d0c71f03b1beac5
Jan Forberg <janforberg@pop-os.localdomain>
2022-01-19 17:21:18 +0100

    fixed sparql cors

--- server/app/pages/module.js
+++ server/app/pages/module.js
@@ -1,5 +1,6 @@
 var sparql = require('../common/queries/sparql');
 var request = require('request');
+var cors = require('cors');
 
 const ServerUtils = require('../common/utils/server-utils.js');
 const DatabusCache = require('../common/databus-cache');
@@ -56,12 +57,12 @@ module.exports = function (router, protector) {
     }
   });
 
-  router.get('/system/sparql', function (req, res) {
+  router.get('/system/sparql', cors(), function (req, res) {
     var url = `${process.env.DATABUS_DATABASE_URL}${req.originalUrl.replace('/system', '')}`;
     request(url).pipe(res);
   });
 
-  router.post('/system/sparql', async function (req, res) {
+  router.post('/system/sparql', cors(), async function (req, res) {
 
     var sparqlEndpoint = `${process.env.DATABUS_DATABASE_URL}/sparql`;
     var query = req.body.query;
